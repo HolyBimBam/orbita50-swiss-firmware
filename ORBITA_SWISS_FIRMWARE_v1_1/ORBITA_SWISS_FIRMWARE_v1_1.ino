4c36222923995559bebf0bc56fb9581cec5af7ec
sennert <info@steffensennert.de>
2024-09-22 18:30:30 +0200

    Update ORBITA_SWISS_FIRMWARE_v1_1.ino

--- ORBITA_SWISS_FIRMWARE_v1_1/ORBITA_SWISS_FIRMWARE_v1_1.ino
+++ ORBITA_SWISS_FIRMWARE_v1_1/ORBITA_SWISS_FIRMWARE_v1_1.ino
@@ -1,10 +1,37 @@
-// ORBITA SWISS EDITION FIRMWARE
-//
-// written 2024 by STEFFEN SENNERT 
-// for PLAYTRONICA
-//
-// SELECT: RP2040 generic (use variant file)
-// ERIN USES WSW25Q16 FLASH with 2MB
+/* 
+ * **************************************************************
+ * ORBITA50 - SWISS EDITION - FIRMWARE 
+ *
+ * written 2024 by STEFFEN SENNERT 
+ * 
+ * SELECT: RP2040 generic (use variant file from github)
+ * Playtronica Oribta ERIN Board uses WSW25Q16 FLASH with 2MB
+ *
+ * **************************************************************
+ * 
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2024 Steffen Sennert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
 
 
 
